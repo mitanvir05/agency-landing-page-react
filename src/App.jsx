f7c93a135e010ab220cfd9ada84532dcc15cb561
Md. Muhaimin Islam Tanvir <tanvir.cse05@gmail.com>
2025-05-06 12:18:00 +0600

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header";
+
 function App() {
   return (
-    <>
-      <h1 className="bg-amber-600">Agency Web</h1>
-    </>
+    <div className="bg-gray-100 min-h-screen flex flex-col">
+      <Header/>
+    </div>
   );
 }
 
