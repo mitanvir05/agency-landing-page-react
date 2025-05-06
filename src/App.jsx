ad8e34973c4d07003a99ddaa7cde5a43315c638d
Md. Muhaimin Islam Tanvir <tanvir.cse05@gmail.com>
2025-05-06 13:39:39 +0600

    faq added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import CompaniesLogo from "./components/CompaniesLogo";
+import FAQ from "./components/FAQ";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <Hero/>
       <CompaniesLogo/>
+      <FAQ/>
       <TrialSection/>
       <Footer/>
     </div>
