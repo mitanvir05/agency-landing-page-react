dcfd17958633c754e807245c533d6e2616ef5238
Md. Muhaimin Islam Tanvir <tanvir.cse05@gmail.com>
2025-05-06 12:49:04 +0600

    hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col">
       <Header/>
+      <Hero/>
     </div>
   );
 }
